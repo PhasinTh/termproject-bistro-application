24c899259b5be2c127c5b442c78993b849af0991
Lazy Bear <c0mzero314159265@gmail.com>
2022-11-26 18:11:36 +0700

    nothing

--- member-svc/src/main.ts
+++ member-svc/src/main.ts
@@ -7,14 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(options);
+  app.enableCors();
 
   // setup global
   app.useGlobalPipes(new ValidationPipe());
